5ab76929709d73445dd051eba74e1fe57993fe01
Hugo Pascoal <hugo_pascoal_@hotmail.com>
2022-04-18 21:21:07 -0300

    bug persistente ainda

--- app/Jobs/CompanyCreated.php
+++ app/Jobs/CompanyCreated.php
@@ -15,17 +15,25 @@ class CompanyCreated implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-
     protected $email;
 
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
     public function __construct(string $email)
     {
         $this->email = $email;
     }
 
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
     public function handle()
     {
-        Mail::to($this->email)
-            ->send(new WelcomeNewCompany());
+        
     }
 }
